PeteyMcFly/Pokemon_Super_Crystal

Add level caps for hardmode

wram.asm
@@ -1648,6 +1648,7 @@ wTempDVSpc:: db ; cfdd
 
 wRandomFailure:: db ; cfde
 wAISwitchedInLock:: db ; cfdf
+wLevelCap:: db ; cfe0
 
 SECTION "WRAM 1", WRAMX
 

monhacks/supercrystal

wram.asm
@@ -1650,6 +1650,7 @@ wRandomFailure:: db ; cfde
 wAISwitchedInLock:: db ; cfdf
 wFarArrayBank:: db ; cfe0
 wEnemyPreviousStatus:: db ; cfe1
+wLevelCap:: db ; cfe2
 
 SECTION "WRAM 1", WRAMX
 